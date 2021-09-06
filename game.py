2646593ab43ad8856363c7720b4ad5b15d711476
gabriel-legros <gabriel.legros@gmail.com>
2021-09-06 12:39:35 -0400

    Added board and piece structure

--- game.py
+++ game.py
@@ -5,10 +5,13 @@ import piece
 
 class Game():
     def __init__(self):
-        self.board = np.zeros((23,10)) #Game board extends above y = 20 to allow pieces to exist there
+        self.board = np.zeros((23,10)) #Game board extends internally above y = 20 to allow pieces to exist there
+        self.boardandpiece = np.zeros((2,20,10)) #3D array.  0 for a dead block, 1 for for a block from the active piece
         self.is_piece_active = False;
+        
     def update(self):
         if(self.is_piece_active == False):
+            self.is_piece_active = True;
             self.active_piece = piece.Piece(random.randint(0,6));
             self.piecex = 4;
             self.piecey = 22;
@@ -17,25 +20,26 @@ class Game():
             if(self.checkcollision()):
                 self.piecey += 1;
                 self.mergepiece();
-              
+        #Updating the visible board
+        for i in range(0,10):
+            for j in range(0,20):
+                self.boardandpiece[0,j,i] = self.board[j,i];
+        for i in range(0, np.size(self.active_piece.data,1)):
+            for j in range(0,np.size(self.active_piece.data,0)):
+                if(self.piecey-i < 20):
+                    self.boardandpiece[1,self.piecey-i, self.piecex+j] = self.active_piece.data[j,i]
         return;
         
     def checkcollision(self): #Check if a piece collides with board with its current coordinates
         collisionresult = 0;
         for i in range(0, np.size(self.active_piece.data,1)):
             for j in range(0,np.size(self.active_piece.data,0)):
-                collisionresult += self.board[self.piecey-i, self.piecex-j]*self.active_piece.data[j,i];
+                collisionresult += self.board[self.piecey-i, self.piecex+j]*self.active_piece.data[j,i];
         return collisionresult;
         
     def mergepiece(self): #Merge the piece with the board
+        self.is_piece_active = False;
         for i in range(0, np.size(self.active_piece.data,1)):
             for j in range(0,np.size(self.active_piece.data,0)):        
-                self.board[self.piecey-i, self.piecex-j] = self.active_piece.data[j,i]
+                self.board[self.piecey-i, self.piecex+j] = self.active_piece.data[j,i]
         return;
-        
-    def render(self):
-        return;
-        
-game = Game();
-game.update();
-print(game.checkcollision())
