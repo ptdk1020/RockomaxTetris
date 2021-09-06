b3c9a7d4f1c4c38aa37edd54a8ab2c85221c711f
gabriel-legros <gabriel.legros@gmail.com>
2021-09-06 13:31:09 -0400

    Rendering Tetris pieces

--- app.py
+++ app.py
@@ -1,20 +1,41 @@
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.uix.widget import Widget
 from kivy.config import Config
+from kivy.graphics import Color, Rectangle
 
+import game
 
 Config.set('graphics', 'width', '700')
 Config.set('graphics', 'height', '700')
 
+tetrisGame = game.Game();
 
-class TetrisGame(Widget):
-    pass
-
+class Time(Widget):
+    def update(self, *args):
+        self.canvas.clear();
+        with self.canvas:
+            for i in range(0,10):
+                for j in range(0,20):
+                    # Add a red color
+                        if(tetrisGame.boardandpiece[0,j,i] == 1):
+                            Color(0, 1.0, 0)
+                            # Add a square for the active Tetris piece blocks
+                            Rectangle(pos=(200+30*i, 50+30*j), size=(30, 30))
+            for i in range(0,10):
+                for j in range(0,20):
+                    # Add a red color
+                        if(tetrisGame.boardandpiece[1,j,i] == 1):
+                            Color(1., 0, 0)
+                            # Add a square for the active Tetris piece blocks
+                            Rectangle(pos=(200+30*i, 50+30*j), size=(30, 30))
 
 class TetrisApp(App):
     def build(self):
-        return TetrisGame()
+        tetrisapp = Time()
+        Clock.schedule_interval(tetrisapp.update, 1)
+        Clock.schedule_interval(tetrisGame.update,1)
+        return tetrisapp
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     TetrisApp().run()
